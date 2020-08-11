8ec0199904b8be15f51c2bec8edec21d31221f09
ﾓｯﾁｰｻｧﾝ <67029618+unagimochimochi@users.noreply.github.com>
2020-08-11 16:55:16 +0900

    20200811: 位置情報取得に苦戦
    実機で位置情報が取得できなくなった。まだ成功しないが一応コミット。

--- varmeets/varmeets/MapViewController.swift
+++ varmeets/varmeets/MapViewController.swift
@@ -15,36 +15,16 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
     
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
-    /* トラッキングモード 多少不具合あり
-     let ImageHeadingUp :UIImage? = UIImage(named:"HeadingupButton")
-     let ImageNorthUp :UIImage? = UIImage(named:"NorthupButton")
-     @IBOutlet var trackingButton: UIButton!
-     @IBAction func trackingBtnTouchDown(_ sender: AnyObject) {
-     switch mapView.userTrackingMode {
-     
-     case .follow :
-     mapView.userTrackingMode = .followWithHeading
-     trackingButton.setImage(ImageHeadingUp, for: .normal)
-     break
-     
-     case .followWithHeading :
-     mapView.userTrackingMode = .none
-     trackingButton.setImage(ImageHeadingUp, for: .normal)
-     break
-     
-     default:
-     mapView.userTrackingMode = .follow
-     trackingButton.setImage(ImageNorthUp, for: .normal)
-     break
-     }
-     }
-     */
+    var locationManager: CLLocationManager!
+    var pointAno: MKPointAnnotation = MKPointAnnotation()
     
     @IBOutlet var longPressGesRec: UILongPressGestureRecognizer!
     // UILongPressGestureRecognizerのdelegate：ロングタップを検出する
     @IBAction func mapViewDidLongPress(_ sender: UILongPressGestureRecognizer) {
         // ロングタップ開始
         if sender.state == .began {
+            // ロングタップ開始時に古いピンを削除する
+            mapView.removeAnnotation(pointAno)
         }
         // ロングタップ終了（手を離した）
         else if sender.state == .ended {
@@ -59,21 +39,35 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
             
             let distance = calcDistance(mapView.userLocation.coordinate, center)
             print("distance: " + distance.description)
+            
+            // ピンに表示する文字列を生成する
+            var str: String = Int(distance).description
+            str = str + " m"
+            
+            if pointAno.title != str {
+                // ピンまでの距離に変化があればtitleを更新する
+                pointAno.title = str
+                mapView.addAnnotation(pointAno)
+            }
+            
+            // ロングタップを検出した位置にピンを立てる
+            pointAno.coordinate = center
+            mapView.addAnnotation(pointAno)
         }
     }
     
-    var locationManager: CLLocationManager!
-    
-    // ？？？
+    // 位置情報取得の許可を得る
     func setupLocationManager() {
         locationManager = CLLocationManager()
         guard let locationManager = locationManager else {
             return
         }
-        locationManager.requestWhenInUseAuthorization()
+        // locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
         
         let status = CLLocationManager.authorizationStatus()
-        if status == .authorizedWhenInUse {
+        // if status == .authorizedWhenInUse {
+        if status == .authorizedAlways {
             locationManager.delegate = self
             locationManager.distanceFilter = 10
             locationManager.startUpdatingLocation()
@@ -93,7 +87,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
     
     // 地図の中心位置の更新関数
     func updateCurrentPos(_ coordinate:CLLocationCoordinate2D) {
-        var region:MKCoordinateRegion = mapView.region
+        var region: MKCoordinateRegion = mapView.region
         region.center = coordinate
         mapView.setRegion(region,animated:true)
     }
@@ -108,7 +102,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
         textField.delegate = self
         
         // キーボード以外をtapした際のアクションをviewに仕込む
-        let hideTap : UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKyeoboard))
+        let hideTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKyeoboard))
         self.view.addGestureRecognizer(hideTap)
         
         // delegateを登録する
@@ -127,8 +121,29 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
         self.view.endEditing(true)
     }
     
+    // 2点間の距離(m)を算出する
+    func calcDistance(_ a: CLLocationCoordinate2D, _ b: CLLocationCoordinate2D) -> CLLocationDistance {
+        let aLoc: CLLocation = CLLocation(latitude: a.latitude, longitude: a.longitude)
+        let bLoc: CLLocation = CLLocation(latitude: b.latitude, longitude: b.longitude)
+        let dist = bLoc.distance(from: aLoc)
+        return dist
+    }
+    
     // 位置情報更新時に地図の中心位置の変更関数を呼び出す
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        
+        let distance = calcDistance(mapView.userLocation.coordinate, pointAno.coordinate)
+        if (0 != distance) {
+            // ピンに設定する文字列を生成する
+            var str: String = Int(distance).description
+            str = str + " m"
+            
+            if pointAno.title != str {
+                // ピンまでの距離に変化があればtitleを更新する
+                pointAno.title = str
+                mapView.addAnnotation(pointAno)
+            }
+        }
         
         switch mapView.userTrackingMode {
         case .followWithHeading:
@@ -142,14 +157,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
         }
     }
     
-    // 2点間の距離(m)を算出する
-    func calcDistance(_ a: CLLocationCoordinate2D, _ b: CLLocationCoordinate2D) -> CLLocationDistance {
-        let aLoc: CLLocation = CLLocation(latitude: a.latitude, longitude: a.longitude)
-        let bLoc: CLLocation = CLLocation(latitude: b.latitude, longitude: b.longitude)
-        let dist = bLoc.distance(from: aLoc)
-        return dist
-    }
-    /// キーボードの検索ボタン押下時
+    // キーボードの検索ボタン押下時
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         // キーボードを閉じる
         self.view.endEditing(true)
@@ -166,7 +174,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureR
             }
             
             // 地図の中心を設定
-            mapView?.setCenter(loc ,animated:true)
+            mapView?.setCenter(loc, animated:true)
             // 縮尺を設定
             let region = MKCoordinateRegion(center: loc, span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
             
